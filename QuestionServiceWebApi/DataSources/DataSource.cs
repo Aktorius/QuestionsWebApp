cb4a04374d98970dbc28f18eedfd8d73ac619149
Aktorius <miri.tarik@gmail.com>
2017-05-31 13:01:48 +0100

    move returned questionnaire to a DataSource

---
+++ QuestionServiceWebApi/DataSources/DataSource.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using QuestionServiceWebApi.Entities;
+
+namespace QuestionServiceWebApi.DataSources
+{
+    public class DataSource
+    {
+        private static DataSource _instance;
+
+        public static DataSource Current
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new DataSource();
+
+                return _instance;
+            }
+        }
+
+        public Questionnaire Questionnaire { get; set; }
+
+        private DataSource()
+        {
+            Questionnaire = new Questionnaire
+            {
+                QuestionnaireTitle = "Geography Questions",
+                QuestionsText = new List<string>
+                {
+                    "What is the capital of Cuba?",
+                    "What is the capital of France?",
+                    "What is the capital of Poland?",
+                    "What is the capital of Germany?"
+                }
+            };
+        }
+    }
+}
--- QuestionServiceWebApi/Repositories/QuestionRepository.cs
+++ QuestionServiceWebApi/Repositories/QuestionRepository.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using QuestionServiceWebApi.DataSources;
 using QuestionServiceWebApi.Entities;
 using QuestionServiceWebApi.Interfaces;
 
@@ -6,19 +6,10 @@ namespace QuestionServiceWebApi.Repositories
 {
     public class QuestionRepository : IQuestionRepository
     {
+        
         public Questionnaire GetQuestionnaire()
         {
-            return new Questionnaire
-            {
-                QuestionnaireTitle = "Geography Questions",
-                QuestionsText = new List<string>
-                                           {
-                                               "What is the capital of Cuba?",
-                                               "What is the capital of France?",
-                                               "What is the capital of Poland?",
-                                               "What is the capital of Germany?"
-                                           }
-            };
+            return DataSource.Current.Questionnaire;
         }
     }
 }
