d9a35f8d0fad943a7a935e0866e9231d1ea8c7b9
Aktorius <miri.tarik@gmail.com>
2017-05-31 10:47:31 +0100

    Create rest service to consume the api

---
+++ PairingTest.Web/Interfaces/IQuestionnaireRestService.cs
@@ -0,0 +1,9 @@
+﻿using PairingTest.Web.Models;
+
+namespace PairingTest.Web.Interfaces
+{
+    public interface IQuestionnaireRestService
+    {
+        QuestionnaireViewModel Get();
+    }
+}
---
+++ PairingTest.Web/Services/QuestionnaireRestService.cs
@@ -0,0 +1,13 @@
+﻿using PairingTest.Web.Interfaces;
+using PairingTest.Web.Models;
+
+namespace PairingTest.Web.Services
+{
+    public class QuestionnaireRestService : IQuestionnaireRestService
+    {
+        public QuestionnaireViewModel Get()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
