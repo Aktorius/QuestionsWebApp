3f67fa48f778ed58db7e91b25c18a8ec662cfb3c
Aktorius <miri.tarik@gmail.com>
2017-05-31 11:48:25 +0100

    Add questionnaire rest service attribute to controller

--- PairingTest.Web/Controllers/QuestionnaireController.cs
+++ PairingTest.Web/Controllers/QuestionnaireController.cs
@@ -1,10 +1,21 @@
 ﻿using System.Web.Mvc;
+using PairingTest.Web.Interfaces;
 using PairingTest.Web.Models;
+using PairingTest.Web.Services;
 
 namespace PairingTest.Web.Controllers
 {
     public class QuestionnaireController : Controller
     {
+        private readonly IQuestionnaireRestService _questionnaireRestService;
+
+        public QuestionnaireController(IQuestionnaireRestService questionnaireRestService)
+        {
+            _questionnaireRestService = questionnaireRestService;
+        }
+
+        public QuestionnaireController(): this(new QuestionnaireRestService()) { }
+
           /* ASYNC ACTION METHOD... IF REQUIRED... */
 //        public async Task<ViewResult> Index()
 //        {
