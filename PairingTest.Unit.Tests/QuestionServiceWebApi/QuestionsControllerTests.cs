261cbba857d06fc2e0f5f1614ab113b8f80c8e80
Aktorius <miri.tarik@gmail.com>
2017-05-31 13:20:39 +0100

    Amend unit test to test an IHttpActionResult

--- PairingTest.Unit.Tests/QuestionServiceWebApi/QuestionsControllerTests.cs
+++ PairingTest.Unit.Tests/QuestionServiceWebApi/QuestionsControllerTests.cs
@@ -1,27 +1,42 @@
-﻿using NUnit.Framework;
+﻿using System.Web.Http.Results;
+using Moq;
+using NUnit.Framework;
 using PairingTest.Unit.Tests.QuestionServiceWebApi.Stubs;
-using QuestionServiceWebApi;
 using QuestionServiceWebApi.Controllers;
 using QuestionServiceWebApi.Entities;
+using QuestionServiceWebApi.Interfaces;
 
 namespace PairingTest.Unit.Tests.QuestionServiceWebApi
 {
     [TestFixture]
     public class QuestionsControllerTests
     {
+        private Mock<IQuestionRepository> _questionRepositoryMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _questionRepositoryMock = new Mock<IQuestionRepository>();
+        }
+
         [Test]
         public void ShouldGetQuestions()
         {
             //Arrange
             var expectedTitle = "My expected questions";
             var expectedQuestions = new Questionnaire() {QuestionnaireTitle = expectedTitle};
-            var fakeQuestionRepository = new FakeQuestionRepository() {ExpectedQuestions = expectedQuestions};
-            var questionsController = new QuestionsController(fakeQuestionRepository);
+
+            _questionRepositoryMock.Setup(x => x.GetQuestionnaire()).Returns(expectedQuestions);
+            
+            var questionsController = new QuestionsController(_questionRepositoryMock.Object);
 
             //Act
-            var questions = questionsController.Get();
+            var actionResult = questionsController.Get();
+            var response = actionResult as OkNegotiatedContentResult<Questionnaire>;
 
             //Assert
+            Assert.IsNotNull(response);
+            var questions = response.Content;
             Assert.That(questions.QuestionnaireTitle, Is.EqualTo(expectedTitle));
         }
     }
