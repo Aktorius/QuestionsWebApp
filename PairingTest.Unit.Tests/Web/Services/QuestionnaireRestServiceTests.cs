c8b27b86635b6797634f92c00825a284418be45d
Aktorius <miri.tarik@gmail.com>
2017-05-31 10:55:29 +0100

    Implement tests for questionnaire rest service

--- PairingTest.Unit.Tests/Web/Services/QuestionnaireRestServiceTests.cs
+++ PairingTest.Unit.Tests/Web/Services/QuestionnaireRestServiceTests.cs
@@ -1,6 +1,23 @@
-﻿namespace PairingTest.Unit.Tests.Web.Services
+﻿using NUnit.Framework;
+using PairingTest.Web.Services;
+
+namespace PairingTest.Unit.Tests.Web.Services
 {
+    [TestFixture]
     class QuestionnaireRestServiceTests
     {
+        [Test]
+        public void ShouldGetQuestions()
+        {
+            //Arrange
+            var expectedTitle = "My expected quesitons";
+            var questionnaireRestService = new QuestionnaireRestService();
+
+            //Act
+            var result = questionnaireRestService.Get();
+
+            //Assert
+            Assert.That(result.QuestionnaireTitle, Is.EqualTo(expectedTitle));
+        }
     }
 }
